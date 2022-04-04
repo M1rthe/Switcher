861f5a318fbd8d77ea0963ee27f31ae985ca1361
M1rthe <mrfvanzwol@gmail.com>
2022-04-04 15:28:27 +0200

    Copy past voor inputfield van code

---
+++ Assets/Scripts/Misc/CopyPaste.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CopyPaste : MonoBehaviour
+{
+    public void Copy(InputField inputField)
+    {
+        GUIUtility.systemCopyBuffer = inputField.text;
+    }
+
+    public void Paste(InputField inputField)
+    {
+        inputField.text = GUIUtility.systemCopyBuffer;
+    }
+}
