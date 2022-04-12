fc486ab57cc3d82eb35b1ef41471c597de9eafb9
M1rthe <mrfvanzwol@gmail.com>
2022-04-12 13:22:59 +0200

    Mission check voor level 2

---
+++ Assets/Scripts/Misc/CheckMissionSucces/Level2MissionCheck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Level2MissionCheck : MissionCheck
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (GameManager.hostJoin == GameManager.HostJoin.Host)
+        {
+            if (other.CompareTag("Player"))
+            {
+                base.photonView.RPC("Succes", RpcTarget.All, timePassed, timesSwitched);
+            }
+        }
+    }
+
+    [PunRPC]
+    public override void Switched()
+    {
+        base.Switched();
+    }
+}
