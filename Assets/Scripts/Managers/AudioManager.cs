2597eb089c2a3eca431dcb97d170ca46e864f95f
M1rthe <mrfvanzwol@gmail.com>
2022-04-23 19:24:52 +0200

    Added audiomanager

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    Transform[] transforms;
+
+    public void PlayAudio(string audioSourceName)
+    {
+        if (transforms == null) transforms = GetComponentsInChildren<Transform>();
+
+        foreach (Transform transform in transforms)
+        {
+            if (transform.gameObject.name == audioSourceName)
+            {
+                AudioSource[] audioSources = transform.GetComponents<AudioSource>();
+                foreach (AudioSource audioSource in audioSources) audioSource.Play();
+            }
+        }
+    }
+}
